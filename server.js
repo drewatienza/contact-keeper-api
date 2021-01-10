e13adbe3596ccf99f3a89b0943fceafde0a36773
drewatienza <atienza.drew@gmail.com>
2021-01-09 16:24:31 -0800

    created backend routes

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-  res.send(`Express server running on port ${PORT}`)
+  res.json({ msg: 'Welcome to the ContactKeeper API' })
 })
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`)
